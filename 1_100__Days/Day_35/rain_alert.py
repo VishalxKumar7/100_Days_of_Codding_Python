1d0050a326b45c26dcc8d1d2966a08ca34d0b6dd
vishalxkumar7 <vishalxkumar7@gamil.com>
2026-05-10 10:24:44 +0530

    Weather API and sending text if it's going to rain

---
+++ 1_100__Days/Day_35/rain_alert.py
@@ -0,0 +1,47 @@
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.environ.get("api_key")
+account_sid = os.environ.get("account_sid")
+auth_token = os.environ.get("auth_token")
+
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = api_key
+account_sid = account_sid
+auth_token = auth_token
+
+
+weather_params = {
+    "lat": 26.14,
+    "lon": 91.73,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+
+    if int(condition_code) < 600:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body = "It's going to rain today. Remember to bring an Umbrella",
+        from_="+14057836308",
+        to="+917210092776"
+    )
+
+    print(message.status)
